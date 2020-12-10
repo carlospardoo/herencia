3763163cdb3ceb26f5b001a1ab8a2bbfb4a6ccc3
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-12-10 10:55:28 -0500

    Agregado properties para ServicioPersonaBDD

--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,5 +1,11 @@
 package com.clearminds.model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
 import com.clearminds.excepcion.InstanceException;
 import com.clearminds.impl.ServicioPersonaArchivos;
 import com.clearminds.impl.ServicioPersonaBDD;
@@ -9,9 +15,10 @@ public class PersonaManager {
 	private ServicioPersona serv;
 	
 	public PersonaManager() throws InstanceException{
+		String implementa = leerPropiedad("dir");
 		Class<?> clase;
 		try {
-			clase = Class.forName("com.clearminds.impl.ServicioPersonaArchivos");
+			clase = Class.forName(implementa);
 			serv=(ServicioPersona)clase.newInstance();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -26,6 +33,36 @@ public class PersonaManager {
 		
 
 		//this.serv = new ServicioPersonaArchivos();
+	}
+	
+	public static String leerPropiedad(String nombrePropiedad) {
+		String valorPropiedad = null;
+
+		Properties p = new Properties();
+		try {
+			File f=new File(System.getProperty("user.dir")
+			+ "//src//main//java//com//clearminds//propiedades//implementacion.properties");
+			//System.out.println("ruta:"+f.getAbsoluteFile());
+			
+			p.load(new FileReader(f));
+			/*System.getProperty("user.dir")
+			+ "//src//main//java//com//clearminds//cdpo//middleware//conexion.properties"*/
+			
+			valorPropiedad = p.getProperty(nombrePropiedad);
+		} catch (FileNotFoundException e) {
+			System.out.println("Error: Archivo no Encontrado");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("Error: Error al capturar archivo");
+			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Error: ");
+			e.printStackTrace();
+		} finally {
+			p.clear();
+		}
+
+		return valorPropiedad;
 	}
 	
 	public void insertarPersona(Persona persona){
