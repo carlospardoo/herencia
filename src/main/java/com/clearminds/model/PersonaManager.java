bb93f4700137f321d7a3d25acc08c18a8ba50cba
Carlos Pardo <28719833+carlospardoo@users.noreply.github.com>
2020-12-10 10:10:59 -0500

    Corregido ejercicio 3.1

--- src/main/java/com/clearminds/model/PersonaManager.java
+++ src/main/java/com/clearminds/model/PersonaManager.java
@@ -1,16 +1,27 @@
 package com.clearminds.model;
 
+import com.clearminds.impl.ServicioPersonaArchivos;
 import com.clearminds.impl.ServicioPersonaBDD;
 import com.clearminds.interfaces.ServicioPersona;
 
 public class PersonaManager {
 	private ServicioPersona serv;
 	
-	public PersonaManager(ServicioPersonaBDD serv){
-		this.serv = serv;
+	public PersonaManager(){
+		this.serv = new ServicioPersonaBDD();
 	}
 	
 	public void insertarPersona(Persona persona){
 		serv.insertar(persona);
 	}
+
+	public ServicioPersona getServ() {
+		return serv;
+	}
+
+	public void setServ(ServicioPersona serv) {
+		this.serv = serv;
+	}
+	
+	
 }
--- src/main/java/com/clearminds/test/TestManager.java
+++ src/main/java/com/clearminds/test/TestManager.java
@@ -1,14 +1,14 @@
 package com.clearminds.test;
 
 import com.clearminds.impl.ServicioPersonaBDD;
+import com.clearminds.interfaces.ServicioPersona;
 import com.clearminds.model.Persona;
 import com.clearminds.model.PersonaManager;
 
 public class TestManager {
 
 	public static void main(String[] args) {
-		ServicioPersonaBDD spb = new ServicioPersonaBDD();
-		PersonaManager pm = new PersonaManager(spb);
+		PersonaManager pm = new PersonaManager();
 		pm.insertarPersona(new Persona("Carlos", "Pardo", "A202064889"));
 
 	}
